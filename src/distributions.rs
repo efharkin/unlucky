cf631560a188a25d5dfbf21fb883cc39173b7d31
Emerson Harkin <emerson.f.harkin@gmail.com>
2020-09-21 11:23:41 -0400

    Replace q-Binomial with PMF skeleton
    q-Binomial/q-factorial implementation produced nonsensical results and
    is scrapped.
    
    This commit introduces the skeleton for a new, probability mass function
    (PMF) convolution approach. This commit sketches out a
    ProbabilityMassFunction struct and accompanying iterator.

--- src/distributions.rs
+++ src/distributions.rs
@@ -1,144 +1,68 @@
-use num_bigint::{BigUint};
-
 fn roll_distribution(num_dice: u32, num_sides: u32) -> Vec<f64> {
     return vec![];
 }
 
-/// q-analog of the binomial coefficient
-///
-/// # Reference
-///
-/// [Wolfram MathWorld](https://mathworld.wolfram.com/q-BinomialCoefficient.html)
-pub fn q_binomial(n: u64, k: u64, q: u64) -> BigUint {
-    assert!(n >= k, "q-binomial coefficient is undefined for k > n, got {} and {}", k, n);
-    return q_factorial(n, q) / (q_factorial(k, q) * q_factorial(n - k, q));
+#[derive(Clone)]
+pub struct ProbabilityMassFunction {
+    pub values: Vec<u64>,
+    pub probabilities: Vec<f64>,
 }
 
-#[cfg(test)]
-mod q_binomial_coefficient_tests {
-    use super::*;
-
-    static Q_TEST_VALUES: [u64; 5] = [1, 2, 8, 20, 30];
-    static N_TEST_VALUES: [u64; 4] = [2, 4, 6, 8];
-
-    /// Test that the q-binomial coefficients for k=1 and k=n-1 are equal.
-    #[test]
-    fn k_symmetry() {
-        for q in Q_TEST_VALUES.iter() {
-            for n in N_TEST_VALUES.iter() {
-                let k_eq_1 = q_binomial(*n, 1, *q);
-                let k_eq_n_minus_1 = q_binomial(*n, *n - 1, *q);
-                assert_eq!(k_eq_1, k_eq_n_minus_1);
-            }
+impl ProbabilityMassFunction {
+    pub fn with_capacity(capacity: usize) -> ProbabilityMassFunction {
+        ProbabilityMassFunction {
+            values: Vec::<u64>::with_capacity(capacity),
+            probabilities: Vec::<f64>::with_capacity(capacity),
         }
-
     }
 
-    #[test]
-    fn four_choose_two_q() {
-        for q in Q_TEST_VALUES.iter() {
-            let expected_result = 1 + *q + 2 * q.pow(2) + q.pow(3) + q.pow(4);
-            let result = q_binomial(4, 2, *q);
-            assert_eq!(BigUint::from(expected_result), result, "Expected {} for 4 choose 2, q={}, got {}", expected_result, *q, result);
-        }
+    pub fn push(&mut self, value: u64, probability: f64) {
+        self.values.push(value);
+        self.probabilities.push(probability);
+    }
+
+    pub fn iter<'a>(&'a self) -> impl Iterator + 'a {
+        ProbabilityMassFunctionIterator::new(&self)
+    }
+
+    pub fn len(&self) -> usize {
+        assert_eq!(self.values.len(), self.probabilities.len());
+        self.values.len()
     }
 }
 
-/// Compute the q-analog of the factorial
-///
-/// [k]_q! = q^0 * (q^0 + q^1) * ... * (q^0 + ... + q^{k-1})
-///
-/// Equal to the standard factorial for q=1.
-///
-/// # Reference
-///
-/// [Wolfram MathWorld](https://mathworld.wolfram.com/q-Factorial.html)
-///
-pub fn q_factorial(k: u64, q: u64) -> BigUint {
-    let mut running_exp = BigUint::from(1u32);
-    let mut running_product = BigUint::from(1u32);
-    let mut running_sum = BigUint::from(1u32);
-
-    return recursive_q_factorial(k, q, 1, &mut running_exp, &mut running_product, &mut running_sum);
+struct ProbabilityMassFunctionIterator<'a> {
+    pmf: &'a ProbabilityMassFunction,
+    ptr: usize,
 }
 
-fn recursive_q_factorial(
-    k: u64,
-    q: u64,
-    i: u64,
-    running_exp: &mut BigUint,
-    running_product: &mut BigUint,
-    running_sum: &mut BigUint,
-) -> BigUint {
-    if i < k {
-        *running_exp *= q; // Compute q^i
-        *running_sum += &*running_exp; // Compute 1 + q + q^2 + ... + q^i
-        *running_product *= &*running_sum; // Compute (1 + q) * (1 + q + q^2) * ...
-        return recursive_q_factorial(k, q, i + 1, running_exp, running_product, running_sum);
-    } else {
-        // Stop if i = k-1
-        return running_product.clone();
+impl<'a> ProbabilityMassFunctionIterator<'a> {
+    pub fn new(pmf: &'a ProbabilityMassFunction) -> Self {
+        ProbabilityMassFunctionIterator {
+            pmf: pmf,
+            ptr: 0
+        }
     }
 }
 
-#[cfg(test)]
-mod factorial_tests {
-    use super::*;
+/// Iterate over a ProbabilityMassFunction
+///
+/// Each iteration step returns a (value, probability) tuple.
+impl <'a> Iterator for ProbabilityMassFunctionIterator<'a> {
+    type Item = (u64, f64);
 
-    static Q_TEST_VALUES: [u64; 4] = [1, 2, 8, 50];
-
-    #[test]
-    fn k_equal_0() {
-        for q in Q_TEST_VALUES.iter() {
-            let result = q_factorial(0, *q);
-            assert_eq!(
-                result, BigUint::from(1u32),
-                "Expected q-factorial = 1 for k=0, q={}, got {} instead",
-                *q, result
-            );
-        }
-    }
-
-    #[test]
-    fn k_equal_1() {
-        for q in Q_TEST_VALUES.iter() {
-            let result = q_factorial(1, *q);
-            assert_eq!(
-                result, BigUint::from(1u32),
-                "Expected q-factorial = 1 for k=1, q={}, got {} instead",
-                *q, result
-            );
-        }
-    }
-
-    #[test]
-    fn k_equal_2() {
-        for q in Q_TEST_VALUES.iter() {
-            let result = q_factorial(2, *q);
-            assert_eq!(
-                result,
-                BigUint::from(1 + *q),
-                "Expected q-factorial = {} for k=2, q={}, got {} instead",
-                1 + *q,
-                *q,
-                result
-            );
-        }
-    }
-
-    #[test]
-    fn k_equal_4() {
-        for q in Q_TEST_VALUES.iter() {
-            let expected_result = (1 + *q) * (1 + *q + q.pow(2)) * (1 + *q + q.pow(2) + q.pow(3));
-            let result = q_factorial(4, *q);
-            assert_eq!(
-                result, BigUint::from(expected_result),
-                "Expected q-factorial = {} for k=4, q={}, got {} instead",
-                expected_result, *q, result
-            );
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.ptr < self.pmf.len() {
+            let ptr = self.ptr;
+            self.ptr += 1;
+            return Some((self.pmf.values[ptr], self.pmf.probabilities[ptr]));
+        } else {
+            return None;
         }
     }
 }
+
+
 
 #[cfg(test)]
 mod distribution_tests {
